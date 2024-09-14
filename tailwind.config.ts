74e2bc3c75616f802266146e4dae3dc1b24e8a50
Sarthak <sarth.mahajan2000@gmail.com>
2024-09-15 01:21:22 +0530

    created hero section

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,13 +4,26 @@ const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1200px",
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        md: "2rem",
+      }
+    },
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        sans: 'vars(--font-sans)',
+        serif: 'vars(--font-serif)',
       },
     },
   },
